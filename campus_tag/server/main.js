bd80058fb3edc8c867adeaff1d00d92644e9c6b3
Ivan Meza <ivn.mza@gmail.com>
2017-01-19 19:14:05 -0800

    Tested functionality of startup function

--- campus_tag/server/main.js
+++ campus_tag/server/main.js
@@ -1,8 +1,5 @@
 import { Meteor } from 'meteor/meteor';
 
-Issues = new Mongo.Collection("issues");
-
 Meteor.startup(() => {
-  // code to run on server at startup
-  console.log("Server says we have a total of "+ Issues.find().count() + " issues.");
+  console.log("Server says hello");
 });
