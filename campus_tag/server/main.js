c0c76e50f58789dcb05db9b18287fe573b8a252a
Ivan Meza <ivn.mza@gmail.com>
2017-01-19 18:16:16 -0800

    Instantiated Issues collection, server logging amount of issues

--- campus_tag/server/main.js
+++ campus_tag/server/main.js
@@ -1,5 +1,8 @@
 import { Meteor } from 'meteor/meteor';
 
+Issues = new Mongo.Collection("issues");
+
 Meteor.startup(() => {
   // code to run on server at startup
+  console.log("Server says we have a total of "+ Issues.find().count() + " issues.");
 });
