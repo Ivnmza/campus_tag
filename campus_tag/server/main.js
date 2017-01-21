71951f378101be3761cf72018bee91ce068fb1c5
Ivan Meza <ivn.mza@gmail.com>
2017-01-21 13:34:40 -0800

    Database initialized

--- campus_tag/server/main.js
+++ campus_tag/server/main.js
@@ -1,5 +1,7 @@
 import { Meteor } from 'meteor/meteor';
 
 Meteor.startup(() => {
-  console.log("Server says hello");
+  console.log("Server says hello at: " + new Date() );
+  Issues = new Mongo.Collection("issues");
+  console.log(Issues.find().count());
 });
