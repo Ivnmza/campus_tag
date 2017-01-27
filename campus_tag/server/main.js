5ca9415ab3a4b05c84abc2d34dc7c5d9dba3abea
Ivan Meza <ivn.mza@gmail.com>
2017-01-27 11:06:08 -0800

    Startup insert commented out temp

--- campus_tag/server/main.js
+++ campus_tag/server/main.js
@@ -4,10 +4,12 @@ Meteor.startup(() => { // in startup file because we want access from shared var
   if (Meteor.isServer) {
     Images.denyClient();
 
-    Images.load('https://raw.githubusercontent.com/VeliovGroup/Meteor-Files/master/logo.png', {
-      fileName: 'logo.png'
-    });
-
+    /*
+      Images.load('https://raw.githubusercontent.com/VeliovGroup/Meteor-Files/master/logo.png', {
+        fileName: 'logo.png'
+      });
+    */
+    
     Meteor.publish('files.images.all', function () {
       console.log("Here in publish: ")
       return Images.find().cursor;
