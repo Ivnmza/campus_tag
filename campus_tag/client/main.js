e52aec3ebede8931166f5d3ed48d10e19fb36968
Ivan Meza <ivn.mza@gmail.com>
2017-01-27 14:10:49 -0800

    Added descriptionform event for file upload

--- campus_tag/client/main.js
+++ campus_tag/client/main.js
@@ -8,3 +8,10 @@ images:function(){
     return Images.find();         
   }
 });
+
+Template.descriptionForm.events({
+  'change :file': function(event) {
+     console.log("Change event: ", event);
+     console.log("Files: ", event.target.files);  
+  }
+})
