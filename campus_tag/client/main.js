8263002ae47afd187a55cff674cd92b4e2b5e6a7
Ivan Meza <ivn.mza@gmail.com>
2017-01-27 16:30:46 -0800

    Handling file submission and insert into DB

--- campus_tag/client/main.js
+++ campus_tag/client/main.js
@@ -9,15 +9,16 @@ images:function(){
   }
 });
 Template.descriptionForm.helpers({
-})
+});
+
 Template.descriptionForm.events({
-  'change :file': function(event) {
-     console.log("Change event: ", event);
+  'change :file': function(event, template) {
+     
      var $this = $(event.target);
      var input = $this;
+     
      var numFiles = input.get(0).files ? input.get(0).files.length : 1;
      var label = input.val().replace(/\\/g, '/').replace(/.*\//, '');
-
      var text = $this.parents('.input-group').find(':text');
      var log = numFiles > 1 ? numFiles + ' files selected' :label;
 
@@ -26,7 +27,31 @@ Template.descriptionForm.events({
       } else {
         if( log ) alert(log);
       }
+      var files = event.target.files;
+      for (var i = 0, ln = files.length; i < ln; i++) {
+      Images.insert(files[i], function (err, fileObj) {
+        // Inserted new doc with ID fileObj._id, and kicked off the data upload using HTTP
+        if (err){
+             // handle error
+             console.log("Error: ", err);
+          } else {
+             // handle success depending what you need to do
+            console.log("Success");
+          }
+      });
+    }
 
      Session.set('log', log);
+
+
+
+
+     console.log("Change event: ", event);
+     console.log("Files ", event.target.files);
+  },
+  'submit #descForm': function(event) {
+    console.log('Form Submitted');
+    event.preventDefault();
+
   }
-})
+});
